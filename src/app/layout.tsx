f2f852d976b967f2d0d20ed3754c4ce9e5b31646
Wesley Kirkland <wesley@wesleyk.me>
2025-06-24 21:48:02 -0400

    Resolve hydration issue from browser extentions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-200`}
+        suppressHydrationWarning
       >
 
         <Header />
