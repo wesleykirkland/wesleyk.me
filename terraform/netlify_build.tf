28117006eda3f17321542706147ded9ee9aceddb
Wesley Kirkland <wesley@wesleyk.me>
2025-06-26 23:00:48 -0400

    Base Terraform config

---
+++ terraform/netlify_build.tf
@@ -0,0 +1,17 @@
+resource "netlify_site_build_settings" "blog_prd" {
+  site_id = data.netlify_site.wesleyk_me.id
+
+  build_command          = "npm run build"
+  publish_directory      = ".next"
+  production_branch      = "main"
+  branch_deploy_branches = ["main"]
+}
+
+resource "netlify_site_build_settings" "blog_stg" {
+  site_id = data.netlify_site.wesleyk_me.id
+
+  build_command          = "npm run build"
+  publish_directory      = ".next"
+  production_branch      = "main"
+  branch_deploy_branches = ["stage"]
+}
