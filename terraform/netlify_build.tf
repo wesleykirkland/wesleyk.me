dd0513c82fef25f0382626dad4d0b6fae4361a95
Wesley Kirkland <wesley@wesleyk.me>
2025-06-27 12:31:13 -0400

    Base build and Domain - To finish

--- terraform/netlify_build.tf
+++ terraform/netlify_build.tf
@@ -6,12 +6,3 @@ resource "netlify_site_build_settings" "blog_prd" {
   production_branch      = "main"
   branch_deploy_branches = ["main"]
 }
-
-resource "netlify_site_build_settings" "blog_stg" {
-  site_id = data.netlify_site.wesleyk_me.id
-
-  build_command          = "npm run build"
-  publish_directory      = ".next"
-  production_branch      = "main"
-  branch_deploy_branches = ["stage"]
-}
