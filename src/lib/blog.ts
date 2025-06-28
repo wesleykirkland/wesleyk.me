98177813ca1256cedf747ce0bfee60c9d1a313fd
Wesley Kirkland <wesley@wesleyk.me>
2025-06-28 12:28:13 -0400

    Combine regex

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -243,7 +243,7 @@ export function getTagSlug(tag: string): string {
   return tag
     .toLowerCase()
     .replace(/[^a-z0-9]+/g, '-')
-    .replace(/^-|-$/g, '');
+    .replace(/(^-)|(-$)/g, '');
 }
 
 export function getTagFromSlug(slug: string): string | null {
