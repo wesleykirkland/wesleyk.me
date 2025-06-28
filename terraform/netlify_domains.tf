2ebf9bb8f1734352ecb1a590355d85fa74bca52d
Wesley Kirkland <wesley@wesleyk.me>
2025-06-28 11:40:48 -0400

    Removing unsed TF variables

--- terraform/netlify_domains.tf
+++ terraform/netlify_domains.tf
@@ -1,7 +1,6 @@
 resource "netlify_site_domain_settings" "blog" {
-  site_id                      = data.netlify_site.blog.id
-  custom_domain                = "blog.wesleyk.me"
-  domain_aliases               = ["blog-alias.wesleyk.me"]
-  branch_deploy_custom_domain  = "blog-branch.wesleyk.me"
-  deploy_preview_custom_domain = "blog-dp.wesleyk.me"
+  site_id = data.netlify_site.blog.id
+
+  custom_domain                = "wesleyk.me"
+  deploy_preview_custom_domain = "preview.wesleyk.me"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,36 +11,6 @@ variable "region" {
 }
 
 # Netlify Configuration
-variable "site_name" {
-  description = "Name of the Netlify site"
-  type        = string
-  default     = "wesleyk-me"
-}
-
-variable "custom_domain" {
-  description = "Custom domain for the site (e.g., wesleyk.me)"
-  type        = string
-  default     = "wesleyk.me"
-}
-
-# Repository Configuration
-variable "github_repo" {
-  description = "GitHub repository path (owner/repo)"
-  type        = string
-  default     = "wesleykirkland/wesleyk.me"
-}
-
-variable "production_branch" {
-  description = "Git branch to deploy from"
-  type        = string
-  default     = "main"
-}
-
-variable "publish_directory" {
-  description = "Directory to publish (Next.js output)"
-  type        = string
-  default     = "out"
-}
 
 # Build Configuration
 variable "node_version" {
@@ -66,12 +36,6 @@ variable "next_public_tagline" {
   description = "Public tagline for the site"
   type        = string
   default     = "DevOps Engineer & Security Researcher"
-}
-
-variable "next_public_professional_titlenext_public_professional_title" {
-  description = "Public Title"
-  type        = string
-  default     = "Sr. Systems Engineer"
 }
 
 variable "next_public_professional_title" {
@@ -104,132 +68,9 @@ variable "next_public_youtube_playlist_url" {
   default     = "https://www.youtube.com/playlist?list=PL6e0QoXQoTzW_YGyfOmcqGP_wHbiAFmhW"
 }
 
-variable "next_public_site_url" {
-  description = "Public site URL"
-  type        = string
-  default     = "https://wesleyk.me"
-}
-
-# Analytics Configuration
-
-# Email Configuration
-variable "email_host" {
-  description = "SMTP host for email"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "email_port" {
-  description = "SMTP port for email"
-  type        = string
-  default     = "587"
-}
-
-variable "email_user" {
-  description = "SMTP username for email"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "email_pass" {
-  description = "SMTP password for email"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "email_from" {
-  description = "From email address"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "email_to" {
-  description = "To email address for contact form"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "email_tls" {
-  description = "Use TLS for email"
-  type        = string
-  default     = "true"
-}
-
-# Captcha Configuration
-variable "hcaptcha_site_key" {
-  description = "hCaptcha site key"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-variable "hcaptcha_secret" {
-  description = "hCaptcha secret key"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
 # Security Configuration
-variable "content_security_policy" {
-  description = "Content Security Policy header"
-  type        = string
-  default     = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.hcaptcha.com https://hcaptcha.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://js.hcaptcha.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com https://hcaptcha.com https://js.hcaptcha.com; frame-src https://hcaptcha.com https://js.hcaptcha.com; object-src 'none'; base-uri 'self';"
-}
-
-# Notification Configuration
-variable "slack_webhook_url" {
-  description = "Slack webhook URL for deployment notifications"
-  type        = string
-  default     = ""
-  sensitive   = true
-}
-
-# Feature Flags
-variable "enable_forms" {
-  description = "Enable Netlify Forms"
-  type        = bool
-  default     = false
-}
-
-variable "enable_identity" {
-  description = "Enable Netlify Identity"
-  type        = bool
-  default     = false
-}
-
-variable "enable_large_media" {
-  description = "Enable Netlify Large Media"
-  type        = bool
-  default     = false
-}
-
-variable "enable_split_testing" {
-  description = "Enable split testing"
-  type        = bool
-  default     = false
-}
-
-# Performance Configuration
-variable "enable_asset_optimization" {
-  description = "Enable asset optimization (CSS/JS minification)"
-  type        = bool
-  default     = true
-}
-
-variable "enable_image_optimization" {
-  description = "Enable image optimization"
-  type        = bool
-  default     = true
-}
-
-variable "enable_pretty_urls" {
-  description = "Enable pretty URLs (remove .html extension)"
-  type        = bool
-  default     = false
-}
+# variable "content_security_policy" {
+#   description = "Content Security Policy header"
+#   type        = string
+#   default     = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.hcaptcha.com https://hcaptcha.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://hcaptcha.com https://js.hcaptcha.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://www.google-analytics.com; connect-src 'self' https://www.google-analytics.com https://hcaptcha.com https://js.hcaptcha.com; frame-src https://hcaptcha.com https://js.hcaptcha.com; object-src 'none'; base-uri 'self';"
+# }
